b26e98f5dadd3c70b4f61be4d670e66d58df2802
mathesh <matheshyogeswaran@gmail.com>
2023-02-02 07:57:52 +0530

    All projects update

--- src/pages/AllProjects.js
+++ src/pages/AllProjects.js
@@ -1,8 +1,80 @@
-const AllProjects = () =>{
-    return(
-        <div>
-            <h1>All Projects</h1>
+import Card from "../components/Card";
+import NavBar from "../components/Navbar";
+const AllProjects = () => {
+  return (
+    <div>
+      <NavBar />
+      <div className="container mt-5">
+        <div className="row mt-3">
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>{" "}
+          <div className="col-md-3">
+            <Card />
+          </div>{" "}
+          <div className="col-md-3">
+            <Card />
+          </div>{" "}
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
+          <div className="col-md-3">
+            <Card />
+          </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default AllProjects;

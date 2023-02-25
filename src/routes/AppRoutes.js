155a63bb3b94b8665552e47676ce5add6b6f9edc
vjathishwarya2000 <vsjathu2000@gmail.com>
2023-02-25 13:01:01 +0530

    frontend_jathis

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -2,9 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AllProjects from "../pages/AllProjects";
 import Dashboard from "../pages/Dashboard";
+import DashboardPM from "../pages/DashboardPM";
 import Project from "../pages/Project";
+import List from "../pages/List";
+import Lboard from "../pages/Lboard";
 import CreateProject from "../pages/CreateProject";
 import FurtherProjectDetails from "../pages/FurtherProjectDetails";
+
 const AppRoutes = () => {
   return (
     <BrowserRouter>
@@ -12,6 +16,9 @@ const AppRoutes = () => {
         <Route exact path="/" element={<Project></Project>} />
         <Route exact path="/project" element={<Project></Project>} />
         <Route exact path="/dashboard" element={<Dashboard/>} />
+        <Route exact path="/dashboardpm" element={<DashboardPM/>} />
+        <Route exact path="/stats" element={<List/>} />
+        <Route exact path="/lboard" element={<Lboard/>} />
         <Route exact path="/allprojects" element={<AllProjects/>} />
         <Route exact path="/project/:projectId" element={<Project/>}/>
         <Route exact path="/project/createProject" element={<CreateProject/>}/>

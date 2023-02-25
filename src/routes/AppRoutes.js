170ecf0cde0b13c4ece40c41a3befb4d76eac5c6
vjathishwarya2000 <vsjathu2000@gmail.com>
2023-02-25 14:55:08 +0530

    modify-pm project

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AllProjects from "../pages/AllProjects";
 import Dashboard from "../pages/Dashboard";
-import DashboardPM from "../pages/DashboardPM";
+import ProjectPM from "../pages/ProjectPM";
 import Project from "../pages/Project";
 import List from "../pages/List";
 import Lboard from "../pages/Lboard";
@@ -10,14 +10,15 @@ import CreateProject from "../pages/CreateProject";
 import FurtherProjectDetails from "../pages/FurtherProjectDetails";
 import DashboardBA from "../pages/DashboardBA";
 import DashboardQA from "../pages/DashboardQA";
+
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Project></Project>} />
         <Route exact path="/project" element={<Project></Project>} />
+        <Route exact path="/projectpm" element={<ProjectPM></ProjectPM>} />
         <Route exact path="/dashboard" element={<Dashboard/>} />
-        <Route exact path="/dashboardpm" element={<DashboardPM/>} />
         <Route exact path="/stats" element={<List/>} />
         <Route exact path="/lboard" element={<Lboard/>} />
         <Route exact path="/allprojects" element={<AllProjects/>} />

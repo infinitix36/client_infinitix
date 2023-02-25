9e2dc238a3c8bed5f0a140418a624ce6a423f3c7
varakiVakeesan <varakivakeesan23@gmail.com>
2023-02-25 12:44:34 +0530

    BA&QA Dash

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -5,6 +5,8 @@ import Dashboard from "../pages/Dashboard";
 import Project from "../pages/Project";
 import CreateProject from "../pages/CreateProject";
 import FurtherProjectDetails from "../pages/FurtherProjectDetails";
+import DashboardBA from "../pages/DashboardBA";
+import DashboardQA from "../pages/DashboardQA";
 const AppRoutes = () => {
   return (
     <BrowserRouter>
@@ -16,7 +18,8 @@ const AppRoutes = () => {
         <Route exact path="/project/:projectId" element={<Project/>}/>
         <Route exact path="/project/createProject" element={<CreateProject/>}/>
         <Route exact path="/project/furtherProject" element={<FurtherProjectDetails/>}/>
-        
+        <Route exact path="/DashboardBA" element={<DashboardBA />} />
+        <Route exact path="/DashboardQA" element={<DashboardQA/>} />
       </Routes>
     </BrowserRouter>
   );

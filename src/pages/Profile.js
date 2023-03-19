db17248c0c0d26d21d4a93b9a2da67cd4ac331ba
mathesh <matheshyogeswaran@gmail.com>
2023-03-19 13:46:14 +0530

    rating

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,158 +1,36 @@
-import React, { useState, useEffect} from "react";
 import NavBar from "../components/Navbar";
-import "../css/Profile.css";
-import { useDispatch, useSelector,Provider} from "react-redux";
-import { Form, Button, Row, Col } from "react-bootstrap";
-import { updateProfile} from "../actions/userActions";
-import ErrorMessage from "../components/ErrorMessage";
-import Loading from "../components/Loading";
-// import MainScreen from "../components/MainScreen";
+import { Rating } from 'react-simple-star-rating'
+import { useState,useEffect } from "react";
+const Profile = () => {
+  const [rating, setRating] = useState(0)
+  const handleRating = (rate: number) => {
+    setRating(rate)
+
+    // other logic
+  }
+  console.log(rating);
+  // Optinal callback functions
+  // const onPointerEnter = () => console.log('Enter')
+  // const onPointerLeave = () => console.log('Leave')
+  // const onPointerMove = (value: number, index: number) => console.log(value, index)
+
+  return (
+    <div>
+      <NavBar />
+      <br></br>
+      <br></br>
+      <br></br>
+      <Rating
+        onClick={handleRating}
+        // onPointerEnter={onPointerEnter}
+        // onPointerLeave={onPointerLeave}
+        // onPointerMove={onPointerMove}
+        /* Available Props */
+      />
 
 
+    </div>
+  );
+};
 
-const Profile = ({ location, history }) => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [pic, setPic] = useState();
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-    const [picMessage, setPicMessage] = useState();
-  
-    const dispatch = useDispatch();
-  
-    const userLogin = useSelector((state) => state.userLogin);
-    const { userInfo } = userLogin;
-  
-    const userUpdate = useSelector((state) => state.userUpdate);
-    const { loading, error, success } = userUpdate;
-  
-    useEffect(() => {
-      if (!userInfo) {
-        history.push("/");
-      } else {
-        setName(userInfo.name);
-        setEmail(userInfo.email);
-        setPic(userInfo.pic);
-      }
-    }, [history, userInfo]);
-  
-    const postDetails = (pics) => {
-      setPicMessage(null);
-      if (pics.type === "image/jpeg" || pics.type === "image/png") {
-        const data = new FormData();
-        data.append("file", pics);
-        data.append("upload_preset", "intravestr");
-        data.append("cloud_name", "thanos");
-        fetch("https://api.cloudinary.com/v1_1/piyushproj/image/upload", {
-          method: "post",
-          body: data,
-        })
-          .then((res) => res.json())
-          .then((data) => {
-            setPic(data.url.toString());
-            console.log(pic);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      } else {
-        return setPicMessage("Please Select an Image");
-      }
-    };
-  
-    const submitHandler = (e) => {
-      e.preventDefault();
-  
-      dispatch(updateProfile({ name, email, password, pic }));
-    };
-  
-    return (<>
-      <Provider store={"hi"} >
-        
-      <Profile title="EDIT PROFILE">
-      <div>
-           <NavBar />
-            <div>
-            <Row className="profileContainer">
-                <Col md={6}>
-                <Form onSubmit={submitHandler}>
-                    {loading && <Loading />}
-                    {success && (
-                    <ErrorMessage variant="success">
-                        Updated Successfully
-                    </ErrorMessage>
-                    )}
-                    {error && <ErrorMessage variant="danger">{error}</ErrorMessage>}
-                    <Form.Group controlId="name">
-                    <Form.Label>Name</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter Name"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    ></Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId="email">
-                    <Form.Label>Email Address</Form.Label>
-                    <Form.Control
-                        type="email"
-                        placeholder="Enter Email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    ></Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId="password">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Enter Password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    ></Form.Control>
-                    </Form.Group>
-                    <Form.Group controlId="confirmPassword">
-                    <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control
-                        type="password"
-                        placeholder="Confirm Password"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                    ></Form.Control>
-                    </Form.Group>{" "}
-                    {picMessage && (
-                    <ErrorMessage variant="danger">{picMessage}</ErrorMessage>
-                    )}
-                    <Form.Group controlId="pic">
-                    <Form.Label>Change Profile Picture</Form.Label>
-                    <Form.File
-                        onChange={(e) => postDetails(e.target.files[0])}
-                        id="custom-file"
-                        type="image/png"
-                        label="Upload Profile Picture"
-                        custom
-                    />
-                    </Form.Group>
-                    <Button type="submit" varient="primary">
-                    Update
-                    </Button>
-                </Form>
-                </Col>
-                <Col
-                style={{
-                    display: "flex",
-                    alignItems: "center",
-                    justifyContent: "center",
-                }}
-                >
-                <img src={pic} alt={name} className="profilePic" />
-                </Col>
-            </Row>
-            </div>
-        </div>
-      </Profile>
-      </Provider>
-      </>
-    );
-  };
-  
 export default Profile;

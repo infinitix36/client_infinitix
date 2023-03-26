56bd859df5c3d799afd14ca63af5f7381bdf227e
vjathishwarya2000 <vsjathu2000@gmail.com>
2023-03-26 16:43:26 +0530

    pagination

--- src/components/Leaderboard/Pagination/index.js
+++ src/components/Leaderboard/Pagination/index.js
@@ -1,30 +1,62 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { Pagination as BootstrapPagination } from "react-bootstrap";
 import styles from "./styles.module.css";
 
 const Pagination = ({ page, total, limit, setPage }) => {
   const totalPages = Math.ceil(total / limit);
 
-  const onClick = (newPage) => {
-    setPage(newPage + 1);
+  const handlePageChange = (newPage) => {
+    setPage(newPage);
   };
+
+  const paginationItems = [];
+
+  // Add Previous button
+  paginationItems.push(
+    <BootstrapPagination.Prev
+      key="prev"
+      onClick={() => handlePageChange(page - 1)}
+      disabled={page === 1}
+      className={`${styles.page_btn}`}
+    />
+  );
+
+  for (let i = 1; i <= totalPages; i++) {
+    paginationItems.push(
+      <BootstrapPagination.Item
+        key={i}
+        active={i === page}
+        onClick={() => handlePageChange(i)}
+        className={`${styles.page_btn}`}
+      >
+        {i}
+      </BootstrapPagination.Item>
+    );
+  }
+
+  // Add Next button
+  paginationItems.push(
+    <BootstrapPagination.Next
+      key="next"
+      onClick={() => handlePageChange(page + 1)}
+      disabled={page === totalPages}
+      className={`${styles.page_btn}`}
+    />
+  );
 
   return (
     <div className={styles.container}>
-      {totalPages > 0 &&
-        [...Array(totalPages)].map((val, index) => (
-          <button
-            onClick={() => onClick(index)}
-            className={
-              page === index + 1
-                ? `${styles.page_btn} ${styles.active}`
-                : styles.page_btn
-            }
-            key={index}
-          >
-            {index + 1}
-          </button>
-        ))}
+      <BootstrapPagination>{paginationItems}</BootstrapPagination>
     </div>
   );
+};
+
+Pagination.propTypes = {
+  page: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  limit: PropTypes.number.isRequired,
+  setPage: PropTypes.func.isRequired,
 };
 
 export default Pagination;

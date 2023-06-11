2ec17ef533cc904cdf5ffd89f56265eb05976c8c
mathesh <matheshyogeswaran@gmail.com>
2023-06-11 12:34:22 +0530

    create project validation

--- src/pages/CreateProject.js
+++ src/pages/CreateProject.js
@@ -3,9 +3,13 @@ import swal from "sweetalert";
 import axios from "axios";
 import jwt_decode from "jwt-decode";
 import NavBar from "../components/Navbar";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 const CreateProject = () => {
   const data = jwt_decode(JSON.parse(localStorage.getItem("token")))?.userData;
   const [techLeadData, setTechLeadData] = useState([]);
+
   useEffect(() => {
     axios
       .get(process.env.REACT_APP_API_URL + "/users/getTechLead")
@@ -14,76 +18,101 @@ const CreateProject = () => {
       });
   }, []);
 
-  const [projectName, setProjectName] = useState();
-  const [description, setDescription] = useState();
-  const [technology, setTechnology] = useState();
-  const [deadLine, setDeadLine] = useState();
-  const [techLead, setTechLead] = useState();
   const projectManager = data?._id;
-  console.log(techLead);
-  const submitProjectData = (e) => {
-    e.preventDefault();
-    const postData = {
-      projectName: projectName,
-      description: description,
-      technology: technology,
-      deadline: deadLine,
-      techlead: techLead,
-      projectManager: projectManager,
-    };
-    axios
-      .post(
-        process.env.REACT_APP_API_URL + "/projects/addBasicProjDetails",
-        postData
-      )
-      .then((res) => {
-        alert(res.data.message);
-        swal("Project Created", {
-          icon: "success",
-        });
+
+  const validationSchema = Yup.object({
+    projectName: Yup.string().required("Project Name is required"),
+    description: Yup.string().required("Description is required"),
+    technology: Yup.string().required("Technology is required"),
+    deadline: Yup.date()
+      .required("Deadline is required")
+      .min(new Date(), "Deadline must be a future date")
+      .transform((currentValue, originalValue) => {
+        if (originalValue.length === 10) {
+          const [year, month, day] = originalValue.split("-");
+          return new Date(year, month - 1, day);
+        }
+        return originalValue;
       })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
+      .typeError("Invalid date format"),
+    techlead: Yup.string().required("Techlead is required"),
+  });
+
+  const formik = useFormik({
+    initialValues: {
+      projectName: "",
+      description: "",
+      technology: "",
+      deadline: "",
+      techlead: "",
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      const postData = {
+        projectName: values.projectName,
+        description: values.description,
+        technology: values.technology,
+        deadline: values.deadline,
+        techlead: values.techlead,
+        projectManager: projectManager,
+      };
+
+      axios
+        .post(
+          process.env.REACT_APP_API_URL + "/projects/addBasicProjDetails",
+          postData
+        )
+        .then((res) => {
+          alert(res.data.message);
+          swal("Project Created", {
+            icon: "success",
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  });
+
   return (
     <React.Fragment>
       <div>
-        {" "}
         <NavBar />
-        <div className="container mt-3 ">
+        <div className="container mt-3">
           <div className="card shadow shadow-lg">
-            <form onSubmit={submitProjectData}>
+            <form onSubmit={formik.handleSubmit}>
               <div className="card-body mt-3">
                 <div className="row m-2">
-                  {/* Project Name */}
                   <div className="col-md-6">
                     <div className="form-floating mb-3">
                       <input
                         type="text"
                         className="form-control"
                         placeholder="Project Name"
-                        value={projectName}
-                        required
                         id="projectname"
-                        onChange={(e) => setProjectName(e.target.value)}
-                      ></input>
-                      <label for="projectname">Project Name</label>
+                        {...formik.getFieldProps("projectName")}
+                      />
+                      <label htmlFor="projectname">Project Name</label>
+                      {formik.touched.projectName &&
+                      formik.errors.projectName ? (
+                        <div className="error">{formik.errors.projectName}</div>
+                      ) : null}
                     </div>
                   </div>
-                  {/* Description */}
                   <div className="col-md-6">
                     <div className="form-floating mb-3">
                       <input
-                        required
                         type="text"
                         className="form-control"
                         placeholder="Description"
-                        value={description}
                         id="description"
-                        onChange={(e) => setDescription(e.target.value)}
-                      ></input>
-                      <label for="description">Description</label>
+                        {...formik.getFieldProps("description")}
+                      />
+                      <label htmlFor="description">Description</label>
+                      {formik.touched.description &&
+                      formik.errors.description ? (
+                        <div className="error">{formik.errors.description}</div>
+                      ) : null}
                     </div>
                   </div>
                 </div>
@@ -91,31 +120,32 @@ const CreateProject = () => {
                   <div className="col-md-6">
                     <div className="form-floating mb-3">
                       <input
-                        required
                         type="text"
                         className="form-control"
                         placeholder="Technology"
-                        value={technology}
                         id="technology"
-                        onChange={(e) => setTechnology(e.target.value)}
-                      ></input>
-                      <label for="technology">Technology</label>
+                        {...formik.getFieldProps("technology")}
+                      />
+                      <label htmlFor="technology">Technology</label>
+                      {formik.touched.technology && formik.errors.technology ? (
+                        <div className="error">{formik.errors.technology}</div>
+                      ) : null}
                     </div>
                   </div>
-                  {/* DeadLine */}
                   <div className="col-md-6">
                     <div className="form-floating mb-3">
                       <input
-                        required
                         type="text"
                         className="form-control"
                         placeholder="Deadline"
                         onFocus={(e) => (e.target.type = "date")}
-                        value={deadLine}
                         id="deadline"
-                        onChange={(e) => setDeadLine(e.target.value)}
-                      ></input>
-                      <label for="deadline">Select Deadline</label>
+                        {...formik.getFieldProps("deadline")}
+                      />
+                      <label htmlFor="deadline">Select Deadline</label>
+                      {formik.touched.deadline && formik.errors.deadline ? (
+                        <div className="error">{formik.errors.deadline}</div>
+                      ) : null}
                     </div>
                   </div>
                 </div>
@@ -124,22 +154,22 @@ const CreateProject = () => {
                     <div className="form-floating mb-3">
                       <select
                         id="techlead"
-                        required
                         className="form-control"
-                        onChange={(e) => setTechLead(e.target.value)}
+                        {...formik.getFieldProps("techlead")}
                       >
-                        <option selected value="" disabled>
+                        <option value="" disabled>
                           Select Techlead
                         </option>
-                        {techLeadData.map((item) => {
-                          return (
-                            <option value={item._id}>
-                              {item.fname + " " + item.lname}
-                            </option>
-                          );
-                        })}
+                        {techLeadData.map((item) => (
+                          <option key={item._id} value={item._id}>
+                            {item.fname + " " + item.lname}
+                          </option>
+                        ))}
                       </select>
-                      <label for="dep">Select Techlead</label>
+                      <label htmlFor="techlead">Select Techlead</label>
+                      {formik.touched.techlead && formik.errors.techlead ? (
+                        <div className="error">{formik.errors.techlead}</div>
+                      ) : null}
                     </div>
                   </div>
                 </div>
@@ -151,20 +181,14 @@ const CreateProject = () => {
                       className="m-2 btn btn-dark text-white form-control"
                       type="submit"
                       value="Submit Details"
-                    ></input>
+                    />
                   </div>
                   <div className="col-md-6">
                     <input
                       className="m-2 btn btn-dark text-white form-control"
                       type="reset"
                       value="Reset"
-                      onClick={() => {
-                        setProjectName("");
-                        setDescription("");
-                        setTechnology("");
-                        setDeadLine("");
-                        setTechLead("");
-                      }}
+                      onClick={formik.handleReset}
                     />
                   </div>
                 </div>
@@ -173,8 +197,9 @@ const CreateProject = () => {
           </div>
         </div>
       </div>
-      <br></br>
+      <br />
     </React.Fragment>
   );
 };
+
 export default CreateProject;
